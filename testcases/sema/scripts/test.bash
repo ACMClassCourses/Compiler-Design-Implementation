dca1dc94134123545102e52ff836042d84d27a06
Lau Yee-Yu <liuyiyu2003@126.com>
2023-08-08 21:29:51 +0800

    chore: add scripts for test the testcases

---
+++ testcases/sema/scripts/test.bash
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Uasge
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <complier> <testcase>" >&2
+    exit 1
+fi
+
+if [ ! -f $2 ]; then
+    echo "Error: testcase file $1 does not exist" >&2
+    exit 1
+fi
+
+# Get verdict
+# VERDICT = 1 if PASS, 0 if FAIL
+if [ $(grep "Verdict:" $2 | wc -l) -ne 1 ]; then
+    echo "Error: Verdict line not found in '$2'." >&2
+    echo "Please make sure this is a valid testcase file." >&2
+    exit 1
+fi
+VERDICT_STRING=$(grep "Verdict:" $2 | awk '{print $2}')
+if [ $VERDICT_STRING == "Fail" ]; then
+    VERDICT=0
+else 
+    VERDICT=1
+fi
+
+# Run the compiler
+pass() {
+    echo "Passed" >&2
+    exit 0
+}
+fail() {
+    echo "Failed" >&2
+    exit 1
+}
+$1 <$2 > /dev/null 2>&1
+RETURN_CODE=$?
+if [ $RETURN_CODE -ne 0 ]; then
+    if [ $VERDICT -eq 0 ]; then
+        pass
+    else
+        fail
+    fi
+else
+    if [ $VERDICT -eq 0 ]; then
+        fail
+    else
+        pass
+    fi
+fi
---
+++ testcases/sema/scripts/test_all.bash
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# This script should be used in the directory 'testcases/sema'.
+
+error_from_wrong_working_directory() {
+    echo "ERROR: The working directory is not okay!" >&2
+    echo "       Please make sure that you are in the 'testcases/sema' directory," >&2
+    echo "       'scripts/test.bash' is an executable file and 'judgelist.txt' exists." >&2
+    exit 1
+}
+
+print_red_err() {
+    echo -e "\033[31m$1\033[0m" >&2
+}
+
+if [ ! -x scripts/test.bash ]; then
+    echo "ERROR: 'scripts/test.bash' is not avaible." >&2
+    error_from_wrong_working_directory
+elif [ ! -f judgelist.txt ]; then
+    echo "ERROR: 'judgelist.txt' not found." >&2
+    error_from_wrong_working_directory
+fi
+
+# jdudge_one_testscase <testcase>
+judge_one_testcase() {
+    scripts/test.bash $COMPILER $1 > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        print_red_err "Fail to pass testcase: '$1'."
+        return 1
+    else
+        return 0
+    fi
+}
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <complier>" >&2
+    exit 1
+else
+    COMPILER=$1
+    wrong_count=0
+    total_count=0
+    while read line; do
+        (( total_count += 1 ))
+        judge_one_testcase $line
+        if [ $? -ne 0 ]; then
+            (( wrong_count += 1 ))
+        fi
+    done < judgelist.txt
+    if [ $wrong_count -eq 0 ]; then
+        echo "\033[32mPassed all testcases.\033[0m" >&2
+    else
+        echo "$(( total_count - wrong_count ))/$total_count passed, $wrong_count/$total_count failed." >&2
+    fi
+fi
