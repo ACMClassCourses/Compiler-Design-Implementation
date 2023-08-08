81946fb875bc228bb72db19f83f739ff713683d2
Lau Yee-Yu <liuyiyu2003@126.com>
2023-08-09 01:32:34 +0800

    chore(testcases/sema): specify the path to sema in the test-all script

--- testcases/sema/scripts/test_all.bash
+++ testcases/sema/scripts/test_all.bash
@@ -1,32 +1,39 @@
 #!/bin/bash
 
-# This script should be used in the directory 'testcases/sema'.
-# Usage: scripts/test_all.bash <compiler>
+# Usage: scripts/test_all.bash <compiler> <sema_dir>
 # Example:
-#     scripts/test_all.bash '../../mxc -fsyntax-only'
+#     scripts/test_all.bash '../../mxc -fsyntax-only' testcases/sema
 
-error_from_wrong_working_directory() {
-    echo "ERROR: The working directory is not okay!" >&2
-    echo "       Please make sure that you are in the 'testcases/sema' directory," >&2
-    echo "       'scripts/test.bash' is an executable file and 'judgelist.txt' exists." >&2
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <complier> <sema_dir>" >&2
+    echo "       If you need to pass arguments to the compiler, please use" >&2
+    echo "       quotation mark(') to pack the arguments along with the compiler" >&2
+    echo "       command. For example," >&2
+    echo "       $0 './my_compiler -a -b -c' testcases/sema." >&2
     exit 1
-}
+fi
+
+COMPILER=$1
+SEMA_DIR=$2
+if [ ${SEMA_DIR: -1:1} != "/" ]; then
+    SEMA_DIR="$SEMA_DIR/"
+fi
 
 print_red_err() {
     echo -e "\033[31m$1\033[0m" >&2
 }
 
-if [ ! -x scripts/test.bash ]; then
-    echo "ERROR: 'scripts/test.bash' is not avaible." >&2
-    error_from_wrong_working_directory
-elif [ ! -f judgelist.txt ]; then
-    echo "ERROR: 'judgelist.txt' not found." >&2
-    error_from_wrong_working_directory
+if [ ! -x ${SEMA_DIR}scripts/test.bash ]; then
+    echo "ERROR: '${SEMA_DIR}scripts/test.bash' is not avaible." >&2
+    exit 1
+elif [ ! -f ${SEMA_DIR}judgelist.txt ]; then
+    echo "ERROR: '${SEMA_DIR}judgelist.txt' not found." >&2
+    exit 1
 fi
 
 # judge_one_testscase <testcase>
 judge_one_testcase() {
-    scripts/test.bash "$COMPILER" $1 > /dev/null 2>&1
+    ${SEMA_DIR}scripts/test.bash "$COMPILER" $1 > /dev/null 2>&1
     if [ $? -ne 0 ]; then
         print_red_err "Fail to pass testcase: '$1'."
         return 1
@@ -34,27 +41,17 @@ judge_one_testcase() {
         return 0
     fi
 }
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <complier>" >&2
-    echo "       If you need to pass arguments to the compiler, please use" >&2
-    echo "       quotation mark(') to pack the arguments along with the compiler" >&2
-    echo "       command. For example, $0 './my_compiler -a -b -c'." >&2
-    exit 1
-else
-    COMPILER=$1
-    wrong_count=0
-    total_count=0
-    while read line; do
-        (( total_count += 1 ))
-        judge_one_testcase $line
-        if [ $? -ne 0 ]; then
-            (( wrong_count += 1 ))
-        fi
-    done < judgelist.txt
-    if [ $wrong_count -eq 0 ]; then
-        echo -e "\033[32mPassed all testcases.\033[0m" >&2
-    else
-        echo "$(( total_count - wrong_count ))/$total_count passed, $wrong_count/$total_count failed." >&2
+wrong_count=0
+total_count=0
+while read line; do
+    (( total_count += 1 ))
+    judge_one_testcase $SEMA_DIR$line
+    if [ $? -ne 0 ]; then
+        (( wrong_count += 1 ))
     fi
+done < ${SEMA_DIR}judgelist.txt
+if [ $wrong_count -eq 0 ]; then
+    echo -e "\033[32mPassed all testcases.\033[0m" >&2
+else
+    echo "$(( total_count - wrong_count ))/$total_count passed, $wrong_count/$total_count failed." >&2
 fi
